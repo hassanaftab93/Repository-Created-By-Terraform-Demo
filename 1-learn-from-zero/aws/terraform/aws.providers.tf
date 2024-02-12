f85898411922e74934ebaa21b3d3ab4df299e0ab
Hassan.Aftab <hassanaftab.pk@gmail.com>
2024-02-12 13:38:36 +0500

    init aws code

--- 1-learn-from-zero/aws/terraform/aws.providers.tf
+++ 1-learn-from-zero/aws/terraform/aws.providers.tf
@@ -1,11 +1,15 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.36.0"
     }
   }
 }
 
 provider "aws" {
+  region                   = var.region
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "default"
 }
