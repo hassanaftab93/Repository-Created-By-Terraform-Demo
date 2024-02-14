22696c907e3482eb1c61936ad848731474b1c13a
Hassan.Aftab <hassanaftab.pk@gmail.com>
2024-02-14 18:47:49 +0500

    Terragrunt files

--- learn/day2/terraform/terraform.main.tf
+++
@@ -1,5 +0,0 @@
-module "aws_kms_key" {
-  source      = "./modules/aws_kms_key"
-  alias       = "alias/key"
-  environment = var.aws_kms_key_environment
-}
--- learn/day2/terraform/terraform.outputs.tf
+++
@@ -1,7 +0,0 @@
-output "aws_kms_key_alias" {
-  value = module.aws_kms_key.alias
-}
-
-output "aws_kms_key_id" {
-  value = module.aws_kms_key.id
-}
